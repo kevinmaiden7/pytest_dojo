8cc72af75d8c250b07dc7b32d9ed8c4983df9886
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-08-22 21:39:11 -0500

    Add class for executing multiple tests

--- math_package/operations.py
+++ math_package/operations.py
@@ -1,2 +1,6 @@
 def two_power(x):
-    return 2**x
+    return 2 ** x
+
+def truncate(x, y):
+    return x // y
+    
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,12 @@
 import operations as op
 
-def test_two_power():
-    assert op.two_power(6) == 64
+class TestClass:
+
+    def test_two_power_6(self):
+        assert op.two_power(6) == 64
+    
+    def test_two_power_10(self):
+        assert op.two_power(10) == 1024
+
+    def test_truncate(self):
+        assert op.truncate(9, 2) == 4
