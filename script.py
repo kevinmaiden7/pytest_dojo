b9054d3373ae85b3e235396d7659354b7782d176
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-08-22 22:34:50 -0500

    Add script.py

---
+++ script.py
@@ -0,0 +1,9 @@
+import operations as op
+
+def main():
+    print("Execution just started...")
+    for i in range(1, 14):
+        print(f"test two_power for {i}:", op.two_power(i))
+
+if __name__ == "__main__":
+    main()
