bce8e07522a10f4a24634774204618843b0c8cef
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-08-23 19:46:38 -0500

    Add unittest assertions and settings for VS Code

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,12 +1,14 @@
 import operations as op
+import unittest
 
-class TestClass:
+class TestClass(unittest.TestCase):
 
     def test_two_power_6(self):
         assert op.two_power(6) == 64
     
     def test_two_power_10(self):
-        assert op.two_power(10) == 1024
+        value = op.two_power(10)
+        self.assertEqual(value, 1024)
 
     def test_truncate(self):
         assert op.truncate(9, 2) == 4
